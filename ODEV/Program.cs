b8ffcc32767a8e80b5d26f158f7039b4d981d741
Erdem Geleri <81824169+erdamnq@users.noreply.github.com>
2024-01-04 09:51:01 +0300

    Odev

--- ODEV/Program.cs
+++ ODEV/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ODEV
+namespace ODEV
 {
     internal class Program
     {
@@ -13,13 +13,52 @@
             Console.WriteLine("3-Hard");
 
             int right = 3;
+            int point = 100;
 
 
             int level = Convert.ToInt32(Console.ReadLine());
 
             if (level == 1)
             {
-                Console.WriteLine("You need to guess 3 numbers. And you have 3 rights");
+                Console.WriteLine("You need to guess 1 numbers. And you have 3 rights. (In 1-4)");
+                Random rnd = new Random();
+                int number = rnd.Next(5);
+                int unumber;
+                for (int i = 0; i < right; i--)
+                {
+
+                    Console.WriteLine("Guess the number:");
+                    unumber = int.Parse(Console.ReadLine());
+
+                    if (unumber == number)
+                    {
+                        Console.WriteLine("You WON!");
+                        break;
+                    }
+                    else if (unumber != number)
+                    {
+                        Console.WriteLine("Try Again!");
+                        right--;
+                        Console.WriteLine(point-5);
+                        if (right == 0)
+                        {
+                            Console.WriteLine("You Lost!");
+                            break;
+                        }
+                        else
+                        {
+                            point = point - 5;
+                            continue;
+                        }
+                    }
+                }
+
+
+            }
+
+            else if (level == 2)
+            {
+                Console.WriteLine("You need to guess 1 numbers. And you have 3 rights.(In 1-10)");
                 Random rnd = new Random();
                 int number = rnd.Next(10);
                 int unumber;
@@ -38,47 +77,14 @@
                     {
                         Console.WriteLine("Try Again!");
                         right--;
+                        Console.WriteLine(point - 10);
                         if (right == 0)
                         {
                             Console.WriteLine("You Lost!");
                         }
                         else
                         {
-                            continue;
-                        }
-                    }
-                }
-
-
-            }
-
-            else if (level == 2)
-            {
-                Console.WriteLine("You need to guess 3 numbers. And you have 3 rights");
-                Random rnd = new Random();
-                int number = rnd.Next(100);
-                int unumber;
-                for (int i = 0; i < right; i--)
-                {
-
-                    Console.WriteLine("Guess the number:");
-                    unumber = int.Parse(Console.ReadLine());
-
-                    if (unumber == number)
-                    {
-                        Console.WriteLine("You WON!");
-                        break;
-                    }
-                    else if (unumber != number)
-                    {
-                        Console.WriteLine("Try Again!");
-                        right--;
-                        if (right == 0)
-                        {
-                            Console.WriteLine("You Lost!");
-                        }
-                        else
-                        {
+                            point = point - ;
                             continue;
                         }
                     }
@@ -87,9 +93,9 @@
 
             else if (level == 3)
             {
-                Console.WriteLine("You need to guess 3 numbers. And you have 3 rights");
+                Console.WriteLine("You need to guess 1 numbers. And you have 3 rights.(In 0-25)");
                 Random rnd = new Random();
-                int number = rnd.Next(1000);
+                int number = rnd.Next(25);
                 int unumber;
                 for (int i = 0; i < right; i--)
                 {
@@ -106,12 +112,16 @@
                     {
                         Console.WriteLine("Try Again!");
                         right--;
+                        Console.WriteLine(point - 20);
+
                         if (right == 0)
                         {
                             Console.WriteLine("You Lost!");
+                            break;
                         }
                         else
                         {
+                            point = point - 20;
                             continue;
                         }
                     }
